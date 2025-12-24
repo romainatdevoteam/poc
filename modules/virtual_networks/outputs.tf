2dcee32aab5b3e29f5223498c8e08c8dd325b2f9
Romain Rodrigues <romain.rodrigues@devoteam.com>
2025-12-24 10:02:16 +0100

    update this to hub

--- modules/virtual_networks/outputs.tf
+++ modules/virtual_networks/outputs.tf
@@ -1,14 +1,14 @@
 output "vnet_id" {
   description = "L'ID du Virtual Network créé"
-  value       = azurerm_virtual_network.this.id
+  value       = azurerm_virtual_network.hub.id
 }
 
 output "vnet_name" {
   description = "Le nom du Virtual Network créé"
-  value       = azurerm_virtual_network.this.name
+  value       = azurerm_virtual_network.hub.name
 }
 
 output "vnet_address_space" {
   description = "L'espace d'adressage configuré"
-  value       = azurerm_virtual_network.this.address_space
+  value       = azurerm_virtual_network.hub.address_space
 }
