9c16dd4822403fcde40398acdbb47cbccd26a5ba
Romain Rodrigues <romain.rodrigues@devoteam.com>
2025-12-24 14:26:06 +0100

    update output of module vnet_peerings

--- modules/vnet_peerings/outputs.tf
+++ modules/vnet_peerings/outputs.tf
@@ -1,9 +1,9 @@
 output "peering_id" {
   description = "L'ID de la ressource de peering créée"
-  value       = azurerm_virtual_network_peering.this.id
+  value       = azurerm_virtual_network_peering.vnet_peering.id
 }
 
 output "peering_name" {
   description = "Le nom du peering"
-  value       = azurerm_virtual_network_peering.this.name
+  value       = azurerm_virtual_network_peering.vnet_peering.name
 }
