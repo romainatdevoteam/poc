081c207103ce67b105ab0fe781075707694e5180
Romain Rodrigues <romain.rodrigues@devoteam.com>
2026-01-07 11:54:34 +0100

    add replace _ - for secret

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ module "github_runner" {
   }
 
   secret_environment_variables = {
-    "access-token" = var.github_pat_token
+    "ACCESS_TOKEN" = var.github_pat_token
   }
 
   tags = {
--- modules/container_apps/main.tf
+++ modules/container_apps/main.tf
@@ -23,12 +23,12 @@ resource "azurerm_container_app" "app" {
     server   = var.acr_login_server
     identity = var.identity_id
   }
-  
+
   # Déclarer les secrets ici
   dynamic "secret" {
     for_each = var.secret_environment_variables != null ? var.secret_environment_variables : {}
     content {
-      name  = secret.key
+      name  = lower(replace(secret.key, "_", "-"))
       value = secret.value
     }
   }
@@ -54,7 +54,7 @@ resource "azurerm_container_app" "app" {
         for_each = var.secret_environment_variables != null ? var.secret_environment_variables : {}
         content {
           name        = env.key
-          secret_name = env.key
+          secret_name = lower(replace(secret.key, "_", "-"))
         }
       }
     }
