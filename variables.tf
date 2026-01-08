9d90b2bf51292bf714f7d268e6d1c17d38b38ee7
Romain Rodrigues <romain.rodrigues@devoteam.com>
2026-01-08 12:02:48 +0100

    add default value

--- variables.tf
+++ variables.tf
@@ -41,18 +41,33 @@ variable "github_pat_token" {
 variable "github_repo_url" {
   type = string
 }
-
 variable "applications_list" {
   description = "Catalogue des applications à déployer (Map d'objets)"
-  # La clé de la map sera le nom unique de l'application (ex: "runner-prod", "api-backend")
   type = map(object({
-    image_name   = string # Nom de l'image dans l'ACR
-    image_tag    = string # Tag (v1, latest...)
-    cpu          = number # Ex: 0.5 ou 2.0
-    memory       = string # Ex: "1Gi" ou "4Gi"
+    image_name   = string
+    image_tag    = string
+    cpu          = number
+    memory       = string
     min_replicas = number
     max_replicas = number
-    env_vars     = optional(map(string), {}) # Variables spécifiques (facultatif)
-    secrets      = optional(map(string), {}) # Secrets spécifiques (facultatif)
+    env_vars     = optional(map(string), {})
+    secrets      = optional(map(string), {})
   }))
+
+  # --- DÉBUT DE LA VALEUR PAR DÉFAUT ---
+  default = {
+    "demo-web" = {                 # Clé unique (Nom de l'app)
+      image_name   = "demo-web-app" # L'image que nous avons importée
+      image_tag    = "latest"
+      cpu          = 0.25           # Suffisant pour une démo (économique)
+      memory       = "0.5Gi"
+      min_replicas = 1
+      max_replicas = 3              # Autorise le scale-out
+      env_vars     = {
+        "APP_ENVIRONMENT" = "dev"   # Exemple de variable d'environnement
+      }
+      secrets      = {}
+    }
+  }
+  # --- FIN DE LA VALEUR PAR DÉFAUT ---
 }
